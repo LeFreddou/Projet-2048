3231e6aa1c46bfa2a04404740d6b022a7cd07cfb
LeFreddou <95537558+LeFreddou@users.noreply.github.com>
2021-12-04 16:56:32 +0100

    Update Projet-2048-main.cpp

--- Projet-2048-main.cpp
+++ Projet-2048-main.cpp
@@ -1,1 +1,28 @@
 #include "projet-2048.h"
+
+
+int main()
+{
+    srand(time(NULL));
+    Plateau p = plateauInitial();
+    cout << dessine(p);
+    while (not estTermine(p))
+    {
+        int direction;
+        do
+        {
+            cin >> direction;
+        } while ((direction < 0 or direction > 3) or (p.egale(deplacement(p, direction))));
+        p = deplacement(p, direction);
+        cout << dessine(p) << endl
+             << "score : " << p.score << endl;
+    }
+    if (estGagnant(p))
+    {
+        cout << "vous avez gagnez, votre score finale est de : " << p.score;
+    }
+    else
+    {
+        cout << "Vous avez perdu, votre score finale est de : " << p.score;
+    }
+}
